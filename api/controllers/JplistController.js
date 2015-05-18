4b6d2b2b71338ba58fd4f388fccf9c8f8f96e356
Andy Yu <andy.yu@leap.com.au>
2015-05-18 17:20:14 +1000

    -- added the filter logic at backend to work for ajax request of jplist.

--- api/controllers/JplistController.js
+++ api/controllers/JplistController.js
@@ -1,10 +1,35 @@
 module.exports = {
   index: function(req, res, next) {
-    console.log('req.xhr=', req.xhr);
+    //console.log('req.xhr=', req.xhr);
 
-    var sql = "select * from item";
+    /*for (var key in req) {
+      console.log("key=", key);
+    }*/
+    //console.log("req.body=", req.body);
+    var statuses = '';
+    if(req.body.statuses){
+      statuses = JSON.parse(unescape(req.body.statuses));
+    }
+
+    Jplist.getItemsList(statuses, function(err, data){
+      if (err) return next(err);
+
+      //sails.log("data=", data);
+
+      var data = {
+        data: data.data
+      };
+
+      //return res.ok(data); // JSON
+      if (req.xhr) { // xhr
+        data.layout = null;
+      }
+      res.view(data);
+    });
+
+    /*var sql = "select * from item";
     Jplist.query(sql, function(err, data){
-      sails.log("data=", data);
+      //sails.log("data=", data);
 
       var data = {
         data: data
@@ -15,7 +40,7 @@ module.exports = {
         data.layout = null;
       }
       res.view(data);
-    });
+    });*/
 
     /* Jplist.find()
       .exec(function(err, response){
--- api/models/Jplist.js
+++ api/models/Jplist.js
@@ -5,10 +5,198 @@
  * @docs        :: http://sailsjs.org/#!documentation/models
  */
 
+var getFilterQuery = function(status, prevQuery, preparedParams){
+
+  var query = ''
+    ,name = status.name
+    ,data = status.data;
+
+  if(name && data){
+
+    switch(name){
+
+      case 'title-filter':{
+
+        if(data.path && data.value){
+
+          if(prevQuery.indexOf('where') !== -1){
+            query = ' and title like ?? ';
+          }
+          else{
+            query = 'where title like ?? ';
+          }
+
+          preparedParams.push('%' + data.value + '%');
+        }
+      }
+        break;
+
+      case 'desc-filter':{
+
+        if(data.path && data.value){
+
+          if(prevQuery.indexOf('where') !== -1){
+            query = ' and description like ?? ';
+          }
+          else{
+            query = 'where description like ?? ';
+          }
+
+          preparedParams.push('%' + data.value + '%');
+        }
+      }
+        break;
+    }
+  }
+
+  return query;
+};
+
+var getSortQuery = function(status, preparedParams){
+
+  var query = ''
+    ,data = status.data
+    ,order = 'asc';
+
+  if(data && data.path){
+
+    switch(data.path){
+
+      case '.title':{
+        query = 'order by title';
+      }
+        break;
+
+      case '.desc':{
+        query = 'order by description';
+      }
+        break;
+
+      case '.like':{
+        query = 'order by likes';
+      }
+        break;
+    }
+
+    if(query){
+      if(data.order){
+        order = data.order.toLowerCase();
+      }
+
+      if(order !== 'desc'){
+        order = 'asc';
+      }
+
+      query = query + ' ' + order;
+    }
+  }
+
+  return query;
+};
+
+var getPagingQuery = function(status, count, preparedParams){
+
+  var query = ''
+    ,data = status.data
+    ,currentPage = 0
+    ,number = 0;
+
+  if(data){
+
+    currentPage = Number(data.currentPage) || 0;
+    number = Number(data.number) || 0;
+
+    if(count > number){
+      query = 'LIMIT ' + currentPage * number + ', ' + number;
+    }
+  }
+
+  return query;
+};
+
 module.exports = {
   adapter: 'mySqlServer',
   attributes: {
     title: 'string'
+  },
+  getItemsList: function(statuses, cb){
+    //sails.log("statuses=", statuses);
+
+    var json = {}
+      ,query = ''
+      ,status
+      ,filter = ''
+      ,sort = ''
+      ,paging = ''
+      ,pagingStatus = null
+      ,preparedParams = []
+      ,count = 0
+      ,stmt;
+
+    for(var i=0; i<statuses.length; i++){
+
+      //get status
+      status = statuses[i];
+
+      switch(status.action){
+
+        case 'paging':{
+          pagingStatus = status;
+        }
+          break;
+
+        case 'filter':{
+          filter += getFilterQuery(status, filter, preparedParams);
+        }
+          break;
+
+        case 'sort':{
+          sort = getSortQuery(status, preparedParams);
+        }
+          break;
+      }
+    }
+
+    //count database items for pagination
+    query = 'SELECT count(ID) as count FROM `item` ' + filter + ' ' + sort;
+
+    console.log("query1=", query);
+    Jplist.query(query, preparedParams, function(err, results){
+
+      if(!err && _.isArray(results) && results.length > 0){
+        count = results[0].count;
+      }
+
+      if(pagingStatus){
+        paging = getPagingQuery(pagingStatus, count, preparedParams);
+      }
+
+      //init query with sort, filter and pagination
+      query = 'SELECT * FROM `item` ' + filter + ' ' + sort + ' ' + paging;
+      console.log("query 2=", query);
+      Jplist.query(query, preparedParams, function(err, results){
+        if (err){
+          return cb(err, {});
+        }
+
+        //sails.log("results=", results);
+
+        cb(null, {
+          count: count
+          ,data: results
+        });
+      });
+    });
+
+    //var sql = "select * from item";
+    /*Jplist.query(sql, function(err, data){
+      if (err){
+        return cb(err, {});
+      }
+
+      return cb(null, data);
+
+    })*/
   }
 };
 
